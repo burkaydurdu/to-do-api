4a885486b83fbc365221364170d4f0e795c34a03
Burkay Durdu <burkaydurdu@outlook.com>
2020-01-15 01:39:03 +0300

    add not found user case

--- src/migrations/20191228175513-add-states-table.up.sql
+++ src/migrations/20191228175513-add-states-table.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS states(
   user_id text NOT NULL REFERENCES users (id) ON DELETE CASCADE,
   title text,
   content json,
-  created_at date,
-  updated_at date,
+  created_at timestamp,
+  updated_at timestamp,
   all_done boolean DEFAULT false,
   s_order INTEGER);
--- src/to_do_api/db/query.clj
+++ src/to_do_api/db/query.clj
@@ -13,9 +13,10 @@
 (def date (java.util.TimeZone/setDefault (java.util.TimeZone/getTimeZone "UTC")))
 
 (defn serializer-data [data data-keys]
-  (if (vector? data-keys)
-    (select-keys data data-keys)
-    data))
+  (cond
+    (empty? data) nil
+    (vector? data-keys) (select-keys data data-keys)
+    :else data))
 
 (defn- register-user [params]
   (-> (j/insert! db :users (conj {:id (gen-id)} params))
@@ -25,33 +26,32 @@
 (defn get-user-with-token [token]
   (j/query db ["SELECT *FROM users where token = ?" token] {:result-set-fn first}))
 
+(defn- set-token-in-user [email password]
+  (j/update! db :users {:token (gen-id)} ["email = ? and password = ?" email password]))
+
+(defn- find-user [email password]
+  (-> (j/query db ["SELECT *FROM users where email = ? and password = ?" email password] {:result-set-fn first})
+      (serializer-data [:id :name :email :token])))
+
 (defn- get-user [params]
   (let [email (:email params)
         password (:password params)]
-    (j/update! db :users {:token (gen-id)} ["email = ? and password = ?" email password])
-    (-> (j/query db ["SELECT *FROM users where email = ? and password = ?" email password] {:result-set-fn first})
-        (serializer-data [:id :name :email :token]))))
+    (set-token-in-user email password)
+    (if-let [user (find-user email password)]
+      user
+      (throw (Exception. "User not found!")))))
 
 (defn- get-user-states [user]
   (j/query db ["SELECT *FROM states WHERE user_id = ?" (:id user)]))
 
-(defn- create-state [params]
-  (j/insert! db :states (conj {:id (gen-id)
-                               :created_at (now)} params)))
-
 (defn- create-and-update [params]
   (let [user-id (-> params :user :id)
-        data    (map #(assoc % :user_id user-id) (:body params))
-        _ (println data)]
+        data    (map #(assoc % :user_id user-id) (:body params))]
     (map #(j/execute! db ["INSERT INTO states (id, user_id, title, all_done, s_order, created_at)
-                           VALUES (?,?,?,?,?,?) ON CONFLICT (id)
+                           VALUES (?,?,?,?,?,NOW()::timestamp) ON CONFLICT (id)
                            DO UPDATE SET title = EXCLUDED.title, all_done = EXCLUDED.all_done,
-                           s_order = EXCLUDED.s_order, updated_at = EXCLUDED.created_at;"
-                          (:id %) (:user_id %) (:title %) (:all_done %) (:s_order %) date]) data)))
-
-(defn- update-state [params]
-  (j/update! db :states (conj {:updated_at (now)}
-                              params) ["id = ?" (:id params)]))
+                           s_order = EXCLUDED.s_order, updated_at = NOW()::timestamp;"
+                          (:id %) (:user_id %) (:title %) (:all_done %) (:s_order %) ]) data)))
 
 (defn- delete-state [id]
   (j/delete! db :states ["id = ?" id]))
