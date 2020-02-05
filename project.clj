d05b99ee1c5ee27255ea52184742d6c97657e154
Burkay Durdu <burkaydurdu@outlook.com>
2020-02-05 19:21:52 +0300

    create dockerfile

--- project.clj
+++ project.clj
@@ -4,10 +4,12 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [ring/ring-core "1.6.3"]
-                 [ring/ring-jetty-adapter "1.6.3"]
+                 [ring "1.7.1"]
                  [ring/ring-json "0.5.0"]
                  [ring/ring-defaults "0.3.2"]
+                 [ring-cors "0.1.12"]
+                 [amalloy/ring-gzip-middleware "0.1.3"]
+                 [compojure "1.6.1"]
                  [clj-commons/secretary "1.2.4"]
                  [org.clojure/java.jdbc "0.7.11"]
                  [org.postgresql/postgresql "42.2.9.jre7"]
