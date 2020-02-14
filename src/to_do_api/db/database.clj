911efaff1a41309214a9c28f9f363f5ad6c878af
Burkay Durdu <burkaydurdu@outlook.com>
2020-02-14 12:37:47 +0300

    fix password field

--- src/to_do_api/db/database.clj
+++ src/to_do_api/db/database.clj
@@ -2,6 +2,6 @@
 
 (def db {:dbtype "postgresql"
          :dbname "to_do"
-         :host   (or (System/getenv "POSTGRES_HOST") "localhost")
-         :user   (or (System/getenv "POSTGRES_USER") "burkaydurdu")
-         :pass   (or (System/getenv "POSTGRES_PASSWORD") nil)})
+         :host     (or (System/getenv "POSTGRES_HOST") "localhost")
+         :user     (or (System/getenv "POSTGRES_USER") "burkaydurdu")
+         :password (or (System/getenv "POSTGRES_PASSWORD") nil)})
